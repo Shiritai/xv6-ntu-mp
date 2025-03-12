13faf3dcb7e2e20d7fc933344e4cc3719f665f0e
Tzu-Ching Yang <shingekinocore@gmail.com>
2025-03-13 06:19:04 +0800

    [file] Enable wider range of file resources

--- kernel/param.h
+++ kernel/param.h
@@ -2,9 +2,9 @@
 
 #define NPROC        64  // maximum number of processes
 #define NCPU          8  // maximum number of CPUs
-#define NOFILE       16  // open files per process
+#define NOFILE      200  // open files per process
 #define NFILE       100  // open files per system
-#define NINODE       50  // maximum number of active i-nodes
+#define NINODE      200  // maximum number of active i-nodes
 #define NDEV         10  // maximum major device number
 #define ROOTDEV       1  // device number of file system root disk
 #define MAXARG       32  // max exec arguments
