14c04f5e4617b57c84ce116cc7b2ee231a51efda
Tzu-Ching Yang <shingekinocore@gmail.com>
2025-03-13 09:56:05 +0800

    [slab] Add the template of slab allocator
    Including
    * Definitions of struct slab and struct kmem_cache in slab.h
    * Function signature of create/alloc/free/destroy in slab.h
    * Parameters in param.h
    * Modify Makefile so that slab.c can be compiled

--- kernel/param.h
+++ kernel/param.h
@@ -15,5 +15,14 @@
 #define MAXPATH      128   // maximum file path name
 #define USERSTACK    1     // user stack pages
 
+// MP2 Macros that CANNOT BE CHANGED!
 #define MP2_FILE_MAGIC_N 116
 #define MP2_TEST
+#define MP2_CACHE_MAX_NAME 32 // Max length of the name of kmem_cache object.
+#define MP2_MIN_AVAIL_SLAB 2 // Minimal numbers of available (partial/free) slabs
+#define MP2_SLAB_SIZE      PGSIZE // Currently set to 1 page.
+
+// TODO: Change the following macros if you implement
+//       freelist related safety enhancement features
+#define MP2_FREELIST_HARDENED      0 // enable (1) / disable (0) MP2_FREELIST_HARDENED
+#define MP2_FREELIST_RANDOMIZATION 0 // enable (1) / disable (0) MP2_FREELIST_RANDOMIZATION
