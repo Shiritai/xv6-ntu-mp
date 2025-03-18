2d33c5c05a116e6be9a7ea7867b41a1901fc6318
Tzu-Ching Yang <shingekinocore@gmail.com>
2025-03-18 09:46:19 +0800

    [test] Update master script (mp2.sh)

--- mp2.sh
+++ mp2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(realpath $(dirname $0))
+CONTAINER_NAME=mp2
+RUNNING_DIR=~/runner
+
 if [[ "$1" == "test" ]]; then
     if [[ -n "$2" ]]; then
         from="$2"
@@ -11,6 +15,19 @@ if [[ "$1" == "test" ]]; then
     else
         python3 test/run_mp2.py
     fi
-elif [[ "$1" == "start" ]]; then
-    echo "start"
+elif [[ "$1" == "pull" ]]; then
+    docker pull ntuos/mp2
+elif [[ "$1" == "run" ]]; then
+    docker run --rm -it -v "$(realpath $SCRIPT_DIR)":"/home/student/mp2" \
+        -u student -w /home/student/mp2 \
+        --name $CONTAINER_NAME ntuos/mp2 ./mp2.sh
+elif [[ "$1" == "bash" ]]; then
+    docker run --rm -it -v "$(realpath $SCRIPT_DIR)":"/home/student/mp2" \
+        -u student -w /home/student/mp2 \
+        --name $CONTAINER_NAME ntuos/mp2 bash
+else
+    mkdir -p $RUNNING_DIR
+    cp -r . $RUNNING_DIR
+    cd $RUNNING_DIR
+    python3 test/run_mp2.py
 fi
