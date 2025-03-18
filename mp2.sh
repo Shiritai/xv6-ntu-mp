3a16513cd6a8aa7bff37396d19c4b2f57ad88ae1
Tzu-Ching Yang <shingekinocore@gmail.com>
2025-03-18 11:36:24 +0800

    [test] Modify mp2.sh script

--- mp2.sh
+++ mp2.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(realpath $(dirname $0))
 CONTAINER_NAME=mp2
 RUNNING_DIR=~/runner
 
-if [[ "$1" == "test" ]]; then
+if [[ "$1" == "testcase" ]]; then
     if [[ -n "$2" ]]; then
         from="$2"
         to=$((from + 1))
@@ -17,11 +17,11 @@ if [[ "$1" == "test" ]]; then
     fi
 elif [[ "$1" == "pull" ]]; then
     docker pull ntuos/mp2
-elif [[ "$1" == "run" ]]; then
+elif [[ "$1" == "test" ]]; then
     docker run --rm -it -v "$(realpath $SCRIPT_DIR)":"/home/student/mp2" \
         -u student -w /home/student/mp2 \
         --name $CONTAINER_NAME ntuos/mp2 ./mp2.sh
-elif [[ "$1" == "bash" ]]; then
+elif [[ "$1" == "run" ]]; then
     docker run --rm -it -v "$(realpath $SCRIPT_DIR)":"/home/student/mp2" \
         -u student -w /home/student/mp2 \
         --name $CONTAINER_NAME ntuos/mp2 bash
