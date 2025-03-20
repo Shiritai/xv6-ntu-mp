80be28b9df212f1e5a47e2fe62910a710877f1bc
Tzu-Ching Yang <shingekinocore@gmail.com>
2025-03-20 21:47:30 +0800

    [ci] Update action_grader.sh

--- action_grader.sh
+++ action_grader.sh
@@ -7,30 +7,37 @@ function get_from_score() {
 ./mp2.sh test slab | tee tmp.txt
 SLAB=$(get_from_score cat tmp.txt)
 echo "Slab structure grade: $SLAB"
+echo
 
 ./mp2.sh test func | tee tmp.txt
 FUNC=$(get_from_score cat tmp.txt)
 echo "Functionality test grade: $FUNC"
+echo
 
 thresh=66
 
 if [[ $FUNC -ge $thresh ]]; then
     echo "Functionality test score is at least $thresh, run bonus test"
+    echo
     ./mp2.sh test list | tee tmp.txt
     LIST=$(get_from_score cat tmp.txt)
     echo "Bonus (list api): $LIST"
+    echo
     ./mp2.sh test cache | tee tmp.txt
     CACHE=$(get_from_score cat tmp.txt)
     echo "Bonus (in-cache): $CACHE"
+    echo
     BONUS=$(( LIST + CACHE ))
 else
     echo "Functionality test score is not greater than $thresh, skip bonus test"
+    echo
     BONUS=0
 fi
 
 ./mp2.sh test private | tee tmp.txt
 PRIVATE=$(get_from_score cat tmp.txt)
 echo "Private test grade: $PRIVATE"
+echo
 
 SCORE=$(( SLAB + FUNC + BONUS + PRIVATE ))
 
